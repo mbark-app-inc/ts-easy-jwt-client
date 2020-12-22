b9b2c85811d9ecf24967f9c4adc238865a98addb
Brian Olencki <brian@olencki.com>
2020-12-21 20:24:01 -0500

    Add top level exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export {}
+import TokenNotFoundError from './errors/TokenNotFoundError'
+
+export { EasyJWTNetworker } from './EasyJWTNetworker'
+export { EasyJWTRequest } from './EasyJWTRequest'
+
+export const errors = {
+  TokenNotFoundError
+}
+
+export * as reduxProcess from './redux-process'
---
+++ src/redux-process/index.ts
@@ -0,0 +1,8 @@
+import NetworkError from './errors/NetworkError'
+
+export { ProcessGroupFactory } from './ProcessGroupFactory'
+export { RequestName } from './types/ProcessFactory'
+
+export const errors = {
+  NetworkError
+}
