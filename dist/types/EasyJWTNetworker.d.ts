11a058c8ce772b43eebf70f776073cd14c96920c
Brian Olencki <brian@olencki.com>
2021-04-20 11:23:24 -0400

    Build v1.1.1

--- dist/types/EasyJWTNetworker.d.ts
+++ dist/types/EasyJWTNetworker.d.ts
@@ -1,4 +1,4 @@
 import { IEasyJWTRequest } from '../interfaces/IEasyJWTRequest'
 export declare type EasyJWTNetworkerOptions = {
-  refreshRequest: IEasyJWTRequest
+  refreshRequest?: IEasyJWTRequest
 }
